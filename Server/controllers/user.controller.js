e084f3cefb40308a8e27c84b0f28f97f3efdb068
LovelySehotra <lsehotra12@gmail.com>
2023-07-26 22:46:28 +0530

    make login,logout,getprofile

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -43,14 +43,54 @@ const register = async (req,res)=>{
 })
 
 };
-const login =(req,res)=>{
+const login =async (req,res)=>{
+    try {
+        const{email,password} = req.body;
+    if(!email || !password)
+    {
+        return next(new AppError('All fields are required',400));
+    }
+    const user = await User.findOne({
+        email
+    }).select('+password');
+    if(!user || !user.comparePassword(password))
+    {
+        return next(new AppError('Email or password does not exist',400));
+    }
+    const token = await user.generateJWTToken();
+    user.password = undefined;
+
+    res.cookie('token',token,cookieOption);
+
+    res.status(200).json({
+        success:true,
+        message:'User loggedin successfully',
+        user,
+    });
+    } catch (e) {
+        return next(new AppError(e.message,500));
+    }
 
 };
 const logout =(req,res)=>{
-
+    res.cookie('token',null,{
+        secure:true,
+        maxAge:0,
+        httpOnly:true
+    })
 };
-const getProfile =(req,res)=>{
-
+const getProfile =async (req,res)=>{
+    try {
+        const userId =req.user.id;
+        const user = await User.findById(userId);
+        res.status(200).json({
+            success:true,
+            message:'User details',
+            user
+        });
+    } catch (e) {
+        return next(new AppError('Failed to fetch profile detail'))
+    }
 };
 export {
     register,
