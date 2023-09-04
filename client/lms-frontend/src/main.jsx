cb31f522efb5eaa5a9c0c50cd072a6f041398b58
Lovely Sehotra <lsehotra12@gmail.com>
2023-09-05 00:18:40 +0530

    add react hot toast

--- client/lms-frontend/src/main.jsx
+++ client/lms-frontend/src/main.jsx
@@ -5,11 +5,13 @@ import './index.css';
 // Library import
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Toaster } from 'react-hot-toast';
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <BrowserRouter>
  <App />
+ <Toaster/>
  </BrowserRouter>
     
   
