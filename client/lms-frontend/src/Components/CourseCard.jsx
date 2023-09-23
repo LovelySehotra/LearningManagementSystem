d07ce6f1ccb6da9d714fd07f780f9f70c894d83f
Lovely Sehotra <lsehotra12@gmail.com>
2023-09-23 17:42:05 +0530

    add courseCard

--- client/lms-frontend/src/Components/CourseCard.jsx
+++ client/lms-frontend/src/Components/CourseCard.jsx
@@ -3,31 +3,34 @@ import { useNavigate } from "react-router-dom";
 function CourseCard({ data }) {
     const navigate = useNavigate();
     return (
-        <div>
-{/* CSS is pending */}
-            <div>
+        <div 
+        onClick={()=>navigate("/course/description/",{state:{...data}})}
+            className="text-white w-[22rem] h-[430px] shadow-lg rounded-lg cursor-pointer group overflow-hidden"
+            >
+
+            <div className="overflow-hidden">
 
                 <img
-                    className=""
+                    className=" h-48 w-full rounded-tl-lg group-hover:scale=[1,2] transition-all ease-in-out diration-300"
                     src={data?.thumbnail?.secure_url}
                     alt="course thumbnail" />
-                <div>
-                    <h2>
-                        {data?.title}hello
+                <div className="p-3 space-y-1 text-white">
+                    <h2 className="text-xl font-bold text-yellow-500 line-clamp-2">
+                        {data?.title}
                     </h2>
-                    <p>
-                    {data?.description}
+                    <p className="line-clamp-2">
+                        {data?.description}
                     </p>
-                    <p>
-                       <span>Category:</span> 
-                       {data?.category}
+                    <p className="font-semibold">
+                        <span className="text-yellow-500 font-bold">Category : </span>
+                        {data?.category}
                     </p>
-                    <p>
-                        <span>Total lecture:</span>
+                    <p className="font-semibold">
+                        <span className="text-yellow-500 font-bold">Total lectures : </span>
                         {data?.numberoflectures}
                     </p>
-                    <p>
-                        <span>Instructor :</span>
+                    <p className="font-semibold">
+                        <span className="text-yellow-500 font-bold">Instructor : </span>
                         {data?.createdBy}
                     </p>
                 </div>
